db1d3011e5f78c424c3cb10ee75d25acdfec4ffb
Prathamesh-Paraswar <70166818+Prathamesh-Paraswar@users.noreply.github.com>
2021-09-01 22:13:28 +0530

    Add files via upload

---
+++ contracts/Adoption.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5;
+contract Adoption{
+    address [16] public adopters;
+    function adopt(uint petID) public returns(uint) {
+        require(petID>=0 && petID<=15);
+        adopters[petID]=msg.sender;
+        return petID;
+    }
+    function getAdopters() view public returns(address[16] memory)
+    {
+        return adopters;
+    }
+}
